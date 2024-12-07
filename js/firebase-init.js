0afb7aad63f19a02f8fd42a4f9b80d504c074542
tabrom <tabrom@web.de>
2024-12-07 15:32:23 +0000

    fixing db config

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -17,4 +17,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+const db = getDatabase(app);
+
+export default db;

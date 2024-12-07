a871625a5a187f68cae1691fa77a8e0299e742a7
tabrom <tabrom@web.de>
2024-12-07 14:18:58 +0000

    fixing restructure?

--- js/custom_utils.js
+++ js/custom_utils.js
@@ -1,26 +1,5 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyAWzbSWohRizdXm1_aWV-AqeyCJLjijpIY",
-    authDomain: "oii-leaderboard.firebaseapp.com",
-    databaseURL: "https://oii-leaderboard-default-rtdb.europe-west1.firebasedatabase.app", 
-    projectId: "oii-leaderboard",
-    storageBucket: "oii-leaderboard.firebasestorage.app",
-    messagingSenderId: "50958797668",
-    appId: "1:50958797668:web:8a08503469366d10cb4871",
-    measurementId: "G-5TE6211QMP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
-
-const db = getDatabase(app);
 
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
 
 export function submitScore(name, score, competition, timestamp) {
     return new Promise((resolve, reject) => {
